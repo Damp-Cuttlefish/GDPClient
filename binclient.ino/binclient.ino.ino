f28186b8df78676fef05ad2ddd2fe3d6d4aa04cb
itzyboi <up735597@myport.ac.uk>
2016-01-25 22:10:44 +0000

    added function declarations and global variables
    Declarations added for both the rangefinder code and the wifi.
    Global variables added for all variables that are going to be sent
    during the wifi function.

---
+++ binclient.ino/binclient.ino.ino
@@ -0,0 +1,28 @@
+int wifi();
+int rangefinder();
+
+byte mac[6]; 
+int maxdepth;
+int currentdepth;
+int batterystatus;
+
+void setup()
+{
+  // put your setup code here, to run once:
+
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+
+}
+
+int rangefinder(void)
+{
+}
+
+int wifi()
+{
+}
+
