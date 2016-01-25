dc0f3bbff5959d956fcb5040b6aca92e2a120580
itzyboi <up735597@myport.ac.uk>
2016-01-25 22:39:23 +0000

    Added Variables
    Added more variables we will need, and their descriptions

--- binclient.ino/binclient.ino.ino
+++ binclient.ino/binclient.ino.ino
@@ -1,14 +1,31 @@
+#include <SD.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+/* Function Declarations */
 int wifi();
 int rangefinder();
-
+/* Variables sent to server */
 byte mac[6]; 
-int maxdepth;
-int currentdepth;
-int batterystatus;
+byte maxdepth;
+byte currentdepth;
+byte batterystatus;
+/* Variables recieved from server */
+byte timevariable;
+/* Wifi connect details */
+// char ssid[] = "ssid";
+// char password[] = "password"; // Used as password for WPA, or the Key for WEP.
+// int keyindex; // Key index for WEP encrpytion.
+
 
 void setup()
 {
-  // put your setup code here, to run once:
+WiFi.macAddress(mac); // Saves the devices mac address to mac, used as the UID.
+
 
 }
 
