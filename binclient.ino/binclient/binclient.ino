71422de4c4878e83dd2ca6ff7d44c0399852f4cb
itzyboi <up735597@myport.ac.uk>
2016-01-27 23:04:34 +0000

    WiFi connection code
    Connects to a WiFi network and displays whether or not it connected
    successfully.

--- binclient.ino/binclient/binclient.ino
+++ binclient.ino/binclient/binclient.ino
@@ -17,22 +17,22 @@ byte batterystatus;
 /* Variables recieved from server */
 byte timevariable;
 /* Wifi connect details */
-// char ssid[] = "ssid";
-// char password[] = "password"; // Used as password for WPA, or the Key for WEP.
+char ssid[] = "ssidGoesHere";
+char password[] = "passwordGoesHere"; // Used as password for WPA, or the Key for WEP.
 // int keyindex; // Key index for WEP encrpytion.
+/* On board LED */
+int led = 13;
 
 
 void setup()
 {
 WiFi.macAddress(mac); // Saves the devices mac address to mac, used as the UID.
 
-
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
-
+  wifi();
 }
 
 int rangefinder(void)
@@ -41,5 +41,18 @@ int rangefinder(void)
 
 int wifi()
 {
+  WiFi.begin(ssid,password);                // begin trying to connect to a WiFi network with WPA authentication.
+  while(WiFi.status() != WL_CONNECTED);     // Wait while chip is connecting.
+  while(WiFi.status() == WL_CONNECT_FAILED) // If WiFi connection fails, blink on board LED.
+  {
+   digitalWrite(led, HIGH);
+   delay(1000);
+   digitalWrite(led, LOW);
+   delay(1000); 
+  }
+  while(WiFi.status() == WL_CONNECTED)      // If WiFi connection succeeds, Turn on board LED on.
+  {
+    digitalWrite(led, HIGH);
+  }
 }
 
