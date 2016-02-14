d2b2528cd93a7ad4c522195e015c6ad49d8584e1
itzyboi <up735597@myport.ac.uk>
2016-02-14 14:26:08 +0000

    Fixed Stuff
    Fixed:
    1) the code
    2) cleaned the code

--- binclient.ino/binclient/binclient.ino
+++ binclient.ino/binclient/binclient.ino
@@ -2,8 +2,6 @@
 #include <WiFiClient.h>
 #include <WiFiClientSecure.h> //Does SSL/TLS
 
-#include <SD.h>
-
 /* Function Declarations */
 void wifi();
 int rangefinder();
@@ -15,7 +13,7 @@ byte batterystatus = 1;
 /* Variables recieved from server */
 byte timevariable;
 /* Wifi connect details */
-char* ssid     = "belkin.c23";
+char* ssid     = "";
 char* password = "";
 // int keyindex; // Key index for WEP encrpytion.
 
@@ -23,11 +21,8 @@ WiFiClient client;
 
 void setup()
 {
-  Serial.begin(115200);
   WiFi.macAddress(mac);
-
   wifi();
-
 }
 
 void loop()
@@ -37,6 +32,7 @@ void loop()
 
 int rangefinder(void)
 {
+  
 }
 
 void wifi(void)
@@ -49,39 +45,24 @@ void wifi(void)
   data = String(data + maxdepth);
   data = String(data + "&battlevel=");
   data = String(data + batterystatus);
-  data = String(data + "&submit=Submit");
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.print(ssid);
 
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
-    Serial.print(".");
   }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 
   if (client.connect("yourwaifuaslut.xyz", 80))
   {
-    Serial.println("connected to the damn server");
     client.println("POST /public/GDPServer/ctest.php HTTP/1.1");
     client.println("Host: yourwaifuaslut.xyz");
+    client.println("User-Agent: Arduino");
     client.println("Content-Type: application/x-www-form-urlencoded");
-    client.println("Content-Length: ");
-    client.print(data.length());
+    client.print("Content-Length: ");
+    client.println(data.length());
     client.println();
     client.println(data);
-
-    Serial.println("Done!");
+    client.stop();
   }
-  else
-    Serial.println("Connect to server failed.");
-
 }
