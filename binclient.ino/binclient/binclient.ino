3c357e11fd1c76c8b69ba86128b8aea668c1ca85
itzyboi <up735597@myport.ac.uk>
2016-02-22 21:03:27 +0000

    Merged Rangefinder
    Jon can't filepath, so i had to manually do it.

--- binclient.ino/binclient/binclient.ino
+++ binclient.ino/binclient/binclient.ino
@@ -1,3 +1,6 @@
+#include <SPI.h>
+#include <SD.h>
+
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiClientSecure.h> //Does SSL/TLS
@@ -6,9 +9,8 @@
 void wifi();
 int rangefinder();
 /* Variables sent to server */
-byte mac[6];
-byte maxdepth;
-byte currentdepth;
+int maxdepth;
+int currentdepth;
 byte batterystatus;
 /* Variables recieved from server */
 byte timevariable;
@@ -16,13 +18,19 @@ byte timevariable;
 char* ssid     = "";
 char* password = "";
 int keyindex; // Key index for WEP encrpytion.
+/* Rangefinding Pins */
+#define pwpin 13
+#define rxpin 12
+#define pwr 14
 
 WiFiClient client;
 
 void setup()
 {
-  WiFi.macAddress(mac);
-  wifi();
+  Serial.begin(115200);
+  maxdepth = rangefinder();
+  Serial.println();
+  Serial.println(maxdepth);
 }
 
 void loop()
@@ -32,11 +40,46 @@ void loop()
 
 int rangefinder(void)
 {
-  
+  int pulse = 0;
+
+  pinMode(pwr, OUTPUT);
+  pinMode(pwpin, OUTPUT);
+  pinMode(rxpin, INPUT);
+
+  digitalWrite(pwr, HIGH);      //set pin 6 high
+  delay(250);                   //wait for 250ms for start up to complete
+
+  digitalWrite(rxpin, HIGH);    //set pin 4 high for 1ms
+  delayMicroseconds(20);
+  digitalWrite(rxpin, LOW);
+  delay(200);
+
+  digitalWrite(rxpin, HIGH);    //set pin 4 high for 1ms
+  delayMicroseconds(20);
+  digitalWrite(rxpin, LOW);
+  pulse = pulseIn(pwpin, HIGH); //listen for return signal
+  pulse = pulse / 58;
+  digitalWrite(pwr, LOW);       //set pin 6 low
+  return pulse;
+}
+
+int sd(void)
+{
+  if (digitalRead(/*the pin card detect is on*/1) == 1)
+  {
+    Serial.println("SD card present");
+    SD.begin(/*insert pin the SD CS is connected to*/1);
+
+  }
+  else
+    Serial.println("SD card not detected");
 }
 
 void wifi(void)
 {
+  byte mac[6];
+  WiFi.macAddress(mac);
+
   String data = String("ID=") ;
   data = String(data + mac[0] + mac[1] + mac[2] + mac[3] + mac[4] + mac[5]);
   data = String(data + "&level=");
